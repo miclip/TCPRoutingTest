2d0fd125d1ff8e27d47a0f177fb5c1628617b512
Michael Lipscombe <mlipscombe@outlook.com>
2021-09-01 15:13:50 -0600

    init

---
+++ TCPRoutingClient/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace TCPRoutingClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "127.0.0.1";
+            Int32 port = 13000;
+            string message = "Hi EVERYBODY!!";
+            try
+            {
+                TcpClient client = new TcpClient(server, port);
+
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+                NetworkStream stream = client.GetStream();
+
+                // Send the message to the connected TcpServer.
+                stream.Write(data, 0, data.Length);
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.
+
+                // Buffer to store the response bytes.
+                data = new Byte[256];
+
+                // String to store the response ASCII representation.
+                String responseData = String.Empty;
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Press Enter to continue...");
+            Console.Read();
+        }
+    }
+}
